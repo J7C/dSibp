5d8c8505d0f06dc1fcbaff46282bba4df82b2256
Jiaqi Chen <793970561@qq.com>
2026-04-12 12:33:53 +0800

    Update bubble IBP generation and Kira reduction results

--- codebubble/bubble_ibp_sym.m
+++ codebubble/bubble_ibp_sym.m
@@ -28,9 +28,11 @@ SetDirectory[dir];
 listcal[expr_, i_, j_, n_] := ReplacePart[expr, {i, j} -> expr[[i, j]] + n]
 
 
-(* Top Sector G \:4e94\:6761 IBP\:ff1a\:65f6\:95f4(2)\:3001\:52a8\:91cf(2)\:3001\:4e92\:8865\:52a8\:91cf(1) *)
-ibp[expr_G, 1] := -I P1 expr - expr[[2, 1]] listcal[expr, 2, 1, -1] - listcal[listcal[expr, 1, 1, 1], 3, 1, -1] - listcal[listcal[expr, 1, 3, 1], 3, 2, -1] + KroneckerDelta[expr[[1, 1]] + expr[[1, 2]], 1] * (-1)^(expr[[1, 1]] + 1) * R1[{expr[[1, 3]], expr[[1, 4]]}, {expr[[2, 1]] + expr[[2, 2]] - 2 nu - 1}, {expr[[3, 1]] + 2 nu + 1, expr[[3, 2]]}] + KroneckerDelta[expr[[1, 3]] + expr[[1, 4]], 1] * (-1)^(expr[[1, 3]] + 1) * R2[{expr[[1, 1]], expr[[1, 2]]}, {expr[[2, 1]] + expr[[2, 2]] - 2 nu - 1}, {expr[[3, 1]], expr[[3, 2]] + 2 nu + 1}]
-ibp[expr_G, 2] := -I P2 expr - expr[[2, 2]] listcal[expr, 2, 2, -1] - listcal[listcal[expr, 1, 2, 1], 3, 1, -1] - listcal[listcal[expr, 1, 4, 1], 3, 2, -1] + KroneckerDelta[expr[[1, 1]] + expr[[1, 2]], 1] * (-1)^(expr[[1, 2]] + 1) * R1[{expr[[1, 3]], expr[[1, 4]]}, {expr[[2, 1]] + expr[[2, 2]] - 2 nu - 1}, {expr[[3, 1]] + 2 nu + 1, expr[[3, 2]]}] + KroneckerDelta[expr[[1, 3]] + expr[[1, 4]], 1] * (-1)^(expr[[1, 4]] + 1) * R2[{expr[[1, 1]], expr[[1, 2]]}, {expr[[2, 1]] + expr[[2, 2]] - 2 nu - 1}, {expr[[3, 1]], expr[[3, 2]] + 2 nu + 1}]
+(*   Top Sector G 4\:6761 IBP\:ff1a\:65f6\:95f4(2)\:3001\:52a8\:91cf(2)  *)
+Ncut=1(*Ncut=1 \:4e0d\:5ffd\:7565topsector\:4e2d\:7684R1,  Ncut=0 \:5ffd\:7565R1*);
+Vpm=0 (*Vpm=0\:4e3aV--, Vpm=1\:4e3aV++*);
+ibp[expr_G, 1] := -I (-1)^Vpm P1 expr - expr[[2, 1]] listcal[expr, 2, 1, -1] - listcal[listcal[expr, 1, 1, 1], 3, 1, -1] - listcal[listcal[expr, 1, 3, 1], 3, 2, -1] +  Ncut(KroneckerDelta[expr[[1, 1]] + expr[[1, 2]], 1] * (-1)^(expr[[1, 1]] + Vpm) * R1[{expr[[1, 3]], expr[[1, 4]]}, {expr[[2, 1]] + expr[[2, 2]] - 2 nu - 1}, {expr[[3, 1]] + 2 nu + 1, expr[[3, 2]]}] + KroneckerDelta[expr[[1, 3]] + expr[[1, 4]], 1] * (-1)^(expr[[1, 3]] + Vpm) * R2[{expr[[1, 1]], expr[[1, 2]]}, {expr[[2, 1]] + expr[[2, 2]] - 2 nu - 1}, {expr[[3, 1]], expr[[3, 2]] + 2 nu + 1}])
+ibp[expr_G, 2] := -I (-1)^Vpm P2 expr - expr[[2, 2]] listcal[expr, 2, 2, -1] - listcal[listcal[expr, 1, 2, 1], 3, 1, -1] - listcal[listcal[expr, 1, 4, 1], 3, 2, -1] +  Ncut(KroneckerDelta[expr[[1, 1]] + expr[[1, 2]], 1] * (-1)^(expr[[1, 2]] + Vpm) * R1[{expr[[1, 3]], expr[[1, 4]]}, {expr[[2, 1]] + expr[[2, 2]] - 2 nu - 1}, {expr[[3, 1]] + 2 nu + 1, expr[[3, 2]]}] + KroneckerDelta[expr[[1, 3]] + expr[[1, 4]], 1] * (-1)^(expr[[1, 4]] + Vpm) * R2[{expr[[1, 1]], expr[[1, 2]]}, {expr[[2, 1]] + expr[[2, 2]] - 2 nu - 1}, {expr[[3, 1]], expr[[3, 2]] + 2 nu + 1}])
 (* Momentum IBP 1: eq:momIBP_qdot from note main.tex line 380-400.
    0 = D*B - b1*B - (1/2)*b2*(B + B[{b1-2,b2+2}] - ks^2*B[{b1,b2+2}])
        + B[{n1+1},{a1+1},{b1-1,b2}] + B[{n2+1},{a2+1},{b1-1,b2}]
@@ -43,26 +45,13 @@ ibp[expr_G, 4] := d expr - expr[[3, 2]] expr - 1/2 expr[[3, 1]] (expr + listcal[
 
 
 (* R1 Sector \:4e09\:6761 IBP\:ff1a\:65f6\:95f4\:3001\:52a8\:91cf\:3001\:4e92\:8865\:52a8\:91cf *)
-ibp[expr_R1, 1] := -I P0R1 expr - expr[[2, 1]] listcal[expr, 2, 1, -1] - listcal[listcal[expr, 1, 1, 1], 3, 2, -1] - listcal[listcal[expr, 1, 2, 1], 3, 2, -1]
+ibp[expr_R1, 1] := -I P0R1 (-1)^Vpm  expr - expr[[2, 1]] listcal[expr, 2, 1, -1] - listcal[listcal[expr, 1, 1, 1], 3, 2, -1] - listcal[listcal[expr, 1, 2, 1], 3, 2, -1]
 ibp[expr_R1, 2] := d expr - expr[[3, 1]] expr - 1/2 expr[[3, 2]] (expr + listcal[listcal[expr, 3, 1, -2], 3, 2, 2] - ks^2 listcal[expr, 3, 2, 2]) + 1/2 (listcal[listcal[listcal[expr, 1, 1, 1], 2, 1, 1], 3, 2, -1] + listcal[listcal[listcal[listcal[expr, 1, 1, 1], 2, 1, 1], 3, 1, -2], 3, 2, 1] - ks^2 listcal[listcal[listcal[expr, 1, 1, 1], 2, 1, 1], 3, 2, 1]) + 1/2 (listcal[listcal[listcal[expr, 1, 2, 1], 2, 1, 1], 3, 2, -1] + listcal[listcal[listcal[listcal[expr, 1, 2, 1], 2, 1, 1], 3, 1, -2], 3, 2, 1] - ks^2 listcal[listcal[listcal[expr, 1, 2, 1], 2, 1, 1], 3, 2, 1])
 ibp[expr_R1, 3] := d expr - expr[[3, 2]] expr - 1/2 expr[[3, 1]] (expr + listcal[listcal[expr, 3, 1, 2], 3, 2, -2] - ks^2 listcal[expr, 3, 1, 2]) + listcal[listcal[listcal[expr, 1, 1, 1], 2, 1, 1], 3, 2, -1] + listcal[listcal[listcal[expr, 1, 2, 1], 2, 1, 1], 3, 2, -1]
 
 
 (* ::Section:: *)
 (*Integral Substitutions & Symmetries*)
-
-
-(* \:901a\:7528\:79ef\:5206\:4e0e a0/b0 \:57fa\:7ebf\:4f4d\:79fb *)
-int00G = G[{n1, n2, n3, n4}, {a1, a2}, {b1, b2}];
-int00R1 = R1[{n3, n4}, {a}, {b1, b2}];
-
-repaddab0G = {a1 -> a1 + a10, a2 -> a2 + a20, b1 -> b1 + b10, b2 -> b2 + b20};
-repaddab0R1 = {a -> a + a0, b1 -> b1 + b10, b2 -> b2 + b20};
-
-repab020[expr_] := expr /. {
-  G[c1_, c2_, c3_] :> (G[c1, c2, c3] /. {a10 -> 0, a20 -> 0, a0 -> 0, b10 -> 0, b20 -> 0}),
-  R1[c1_, c2_, c3_] :> (R1[c1, c2, c3] /. {a10 -> 0, a20 -> 0, a0 -> 0, b10 -> 0, b20 -> 0})
-};
 
 
 (* Hankel \:65b9\:7a0b EOM\:ff1a\:628a n=2 \:9012\:5f52\:5230 n=0,1 *)
@@ -80,7 +69,7 @@ id[expr_] := expr /. {
 
 (* \:5bf9\:79f0\:6027\:ff1aTop Sector \:5185\:90e8 + R1 \:5185\:90e8 + R2 \:8f6c R1 *)
 symmetry[expr_] := Module[{test},
-(* R2 -> R1 (Eq 43) *)test=expr /. {R2[{n1_, n2_}, {a_}, {b1_, b2_}] :> R1[{n1, n2}, {a}, {b1, b2}]};
+(* R2 -> R1 (Eq 43) *)test=expr /. {R2[{n1_, n2_}, {a_}, {b1_, b2_}] :> R1[{n1, n2}, {a}, {b2, b1}]};
 (* Top Sector Symmetry *) test=test/.G[{n1_, n2_, n3_, n4_}, {a1_, a2_}, {b1_, b2_}] :> If[a1 > a2, G[{n2, n1, n4, n3}, {a2, a1}, {b1, b2}], G[{n1, n2, n3, n4}, {a1, a2}, {b1, b2}]] /.
   G[{n1_, n2_, n3_, n4_}, {a1_, a2_}, {b1_, b2_}] :> If[b1 > b2, G[{n3, n4, n1, n2}, {a1, a2}, {b2, b1}], G[{n1, n2, n3, n4}, {a1, a2}, {b1, b2}]] /.
   G[{n1_, n2_, n3_, n4_}, {a1_, a2_}, {b1_, b2_}] :> If[a1 == a2 && n1 > n2, G[{n2, n1, n4, n3}, {a2, a1}, {b1, b2}], G[{n1, n2, n3, n4}, {a1, a2}, {b1, b2}]] /.
@@ -101,60 +90,7 @@ reppowerselection = {
 };
 
 
-(* ::Section:: *)
-(*Generating Base IBP Seeds*)
-
-
-int000G = int00G /. repaddab0G;
-int000R1 = int00R1 /. repaddab0R1;
-
-IBPset00G = Table[ibp[int000G, i], {i, 4}];
-IBPset0G = Table[
-  Table[IBPset00G[[i]], {n1, 0, 1}, {n2, 0, 1}, {n3, 0, 1}, {n4, 0, 1}] // repab020 // Flatten // id  // DeleteDuplicates
-  , {i, 4}
-] /. {
-  R1[c1_, c2_, c3_] :> (R1[c1, c2, c3] /. nu -> 0),
-  R2[c1_, c2_, c3_] :> (R2[c1, c2, c3] /. nu -> 0)
-};
-
-IBPset00R1 = Table[ibp[int000R1, i], {i, 3}];
-IBPset0R1 = Table[
-  Table[IBPset00R1[[i]], {n3, 0, 1}, {n4, 0, 1}] // repab020 // Flatten // id  // DeleteDuplicates
-  , {i, 3}
-];
-
-
-(*bubble_top\:68c0\:9a8c\:ff01\:ff01ii=3,4\:65f6\:5747\:5e94\:4e3a0*)
-ibptest[expr_,3]:=d expr+expr[[3,1]]expr+1/2 expr[[3,2]](expr+listcal[listcal[expr,3,1,2],3,2,-2]-ks^2 listcal[expr,3,2,-2])+listcal[listcal[listcal[expr,1,1,1],2,1,1],3,1,1]+listcal[listcal[listcal[expr,1,2,1],2,2,1],3,1,1]+1/2 (listcal[listcal[listcal[expr,1,3,1],2,1,1],3,2,1]+listcal[listcal[listcal[listcal[expr,1,3,1],2,1,1],3,1,2],3,2,-1]-ks^2 listcal[listcal[listcal[expr,1,3,1],2,1,1],3,2,-1])+1/2 (listcal[listcal[listcal[expr,1,4,1],2,2,1],3,2,1]+listcal[listcal[listcal[listcal[expr,1,4,1],2,2,1],3,1,2],3,2,-1]-ks^2 listcal[listcal[listcal[expr,1,4,1],2,2,1],3,2,-1])
-ibptest[expr_,4]:=d expr+expr[[3,2]]expr+1/2 expr[[3,1]](expr+listcal[listcal[expr,3,1,-2],3,2,2]-ks^2 listcal[expr,3,1,-2])+listcal[listcal[listcal[expr,1,3,1],2,1,1],3,2,1]+listcal[listcal[listcal[expr,1,4,1],2,2,1],3,2,1]+1/2 (listcal[listcal[listcal[expr,1,1,1],2,1,1],3,1,1]+listcal[listcal[listcal[listcal[expr,1,1,1],2,1,1],3,1,-1],3,2,2]-ks^2 listcal[listcal[listcal[expr,1,1,1],2,1,1],3,1,-1])+1/2 (listcal[listcal[listcal[expr,1,2,1],2,2,1],3,1,1]+listcal[listcal[listcal[listcal[expr,1,2,1],2,2,1],3,1,-1],3,2,2]-ks^2 listcal[listcal[listcal[expr,1,2,1],2,2,1],3,1,-1])
-Do[
-test1=ibp[int00G, ii]//Simplify;
-test2=(ibptest[G[{n1,n2,n3,n4},{a1,a2},{b1,b2}],ii]/.{b1->-b1,b2->-b2}/.G[c1__,c2_]->G[c1,-c2])//Simplify;
-test1-test2//Simplify//Print;,{ii,3,4}]
-
-
-(* ::Chapter:: *)
-(*IBP Generation & Export*)
-
-
-(* ::Section:: *)
-(*Parameter Setup & IBP Ranges*)
-
-
-(* Top Sector Ranges *)
-aMin = -2; aMax = 3; bMin = 0; bMax = 4;
-
-(* R1 Sector Ranges (Matched or Expanded) *)
-aR1Min = -4; aR1Max = 6; bR1Min = -4; bR1Max = 6;
-
-
-(* Kira \:4fa7\:91c7\:7528\:7684\:53c2\:6570\:66ff\:6362 *)
-(* \:6ce8\:610f\:ff1aTop Sector P1=P2=P0, R1 Sector P0R1 = 2 P0 *)
-repvar = {P1 -> -I k0, P2 -> -I k0, P0R1 -> -2 I k0, a10 -> a0, a20 -> a0, b10 -> b0, b20 -> b0};
-reppara2N = {a0 -> 2 nu, b0 -> -2 nu, d -> 3 - 2 ep, ks -> 1};
-
-
-(* 1D\[RightArrow]3D \:5347\:7ef4\:6620\:5c04 *)
+(* 1D\[RightArrow]3D \:5347\:7ef4\:6620\:5c04----------\:5bf9\:79ef\:5206\:6307\:6807\:4e2da0 b0\:5df2\:88abshift\:6389\:ff08\:5269\:4e0b\:5269\:4e0b\:6574\:6570\:ff09\:7684\:79ef\:5206\:4f7f\:7528\:ff01\:ff01\:ff01 *)
 repdimplus2[expr_] := Module[{res},
   res = expr /. {
     G[c1_, c2_, c3_] :> (
@@ -176,8 +112,73 @@ repdimplus2[expr_] := Module[{res},
 ];
 
 
+(* \:901a\:7528\:79ef\:5206\:4e0e a0/b0 \:57fa\:7ebf\:4f4d\:79fb *)
+int00G = G[{n1, n2, n3, n4}, {a1, a2}, {b1, b2}];
+int00R1 = R1[{n3, n4}, {a}, {b1, b2}];
+
+repaddab0G = {a1 -> a1 + a10, a2 -> a2 + a20, b1 -> b1 + b10, b2 -> b2 + b20};
+repaddab0R1 = {a -> a + a0R, b1 -> b1 + b10, b2 -> b2 + b20};
+
+repab020[expr_] := expr /. {
+  G[c1_, c2_, c3_] :> (G[c1, c2, c3] /. {a10 -> 0, a20 -> 0, a0 -> 0, b10 -> 0, b20 -> 0}),
+  R1[c1_, c2_, c3_] :> (R1[c1, c2, c3] /. {a10 -> 0, a20 -> 0, a0R -> 0, b10 -> 0, b20 -> 0}),
+  R2[c1_, c2_, c3_] :> (R2[c1, c2, c3] /. {a10 -> 0, a20 -> 0, a0R -> 0, b10 -> 0, b20 -> 0})
+};
+
+
+(* Kira \:4fa7\:91c7\:7528\:7684\:53c2\:6570\:66ff\:6362 *)
+(* \:6ce8\:610f\:ff1aTop Sector P1=P2=P0, R1 Sector P0R1 = 2 P0 *)
+repvar = {P1 -> -I k0, P2 -> -I k0, P0R1 -> -2 I k0, a10 -> a0, a20 -> a0, b10 -> b0, b20 -> b0};
+reppara2N = {a0 -> da +2 nu, a0R -> 2 da +2 nu, b0 -> -2 nu, d -> 3 - 2 ep, ks -> 1};
+
+
+(* ::Section:: *)
+(*Generating Base IBP Seeds*)
+
+
+int000G = int00G /. repaddab0G
+int000R1 = int00R1 /. repaddab0R1
+
+IBPset00G = Table[ibp[int000G, i], {i, 4}];
+IBPset0G = Table[
+  Table[IBPset00G[[i]], {n1, 0, 1}, {n2, 0, 1}, {n3, 0, 1}, {n4, 0, 1}] // repab020 // Flatten // id  // DeleteDuplicates
+  , {i, 4}
+] /. {
+  R1[c1_, c2_, c3_] :> (R1[c1, c2, c3] /. nu -> 0),
+  R2[c1_, c2_, c3_] :> (R2[c1, c2, c3] /. nu -> 0)
+}(*bubble\:6536\:7f29\:4f20\:64ad\:5b50\:751f\:6210\:7684R1\:91cc\:6709\:989d\:5916\:7684nu\:56e0\:5b50\:ff0c\:4e0eR1\:51fd\:6570\:65cf\:8bb0\:53f7\:672a\:7edf\:4e00\:ff0c\:7ecf\:8fc7\:8be5\:53d8\:6362\:540e\:7edf\:4e00*)//Simplify;
+
+IBPset00R1 = Table[ibp[int000R1, i], {i, 3}];
+IBPset0R1 = Table[
+  Table[IBPset00R1[[i]], {n3, 0, 1}, {n4, 0, 1}] // repab020 // Flatten // id  // DeleteDuplicates
+  , {i, 3}
+]//Simplify;
+
+
+IBPset0G/._G->1//Simplify//Cases[#,_R1,Infinity]&//DeleteDuplicates
+
+
+test=IBPset0G[[4]]/.{G[c1_,c2_,c3_]->G[Total[c2]+Total[c3]-a1-a2-b1-b2],R1[c1_,c2_,c3_]->R1[Total[c2]+Total[c3]-a1-a2-b1-b2]}//Simplify;
+test1=Table[test//Coefficient[#,G[i]]&,{i,{-2,-1,0,1,2}}];
+test1//Cases[#,_G,Infinity]&//DeleteDuplicates
+test1/.repvar(*/.reppara2N*)//Simplify//MatrixForm
+test=.;test1=.;
+\:9700\:8981\:5c0f\:5fc3\:68c0\:9a8c (-k tau)^{-2 nu-1}
+
+
+(* ::Chapter:: *)
+(*IBP Generation & Export*)
+
+
 (* ::Section:: *)
 (*Generating and Expanding IBP Equations*)
+
+
+(* Top Sector Ranges *)
+aMin = -2; aMax = 3; bMin = 0; bMax = 4;
+
+(* R1 Sector Ranges (Matched or Expanded) *)
+aR1Min = -4; aR1Max = 6; bR1Min = -4; bR1Max = 6;
 
 
 Print["Generating IBP equations..."];
@@ -196,9 +197,10 @@ IBPset1R1 = Join[
   Table[IBPset0R1[[2]] /. repvar /. reppara2N, {a, aR1Min, aR1Max-1}, {b1, bR1Min+2, bR1Max}, {b2, bR1Min+1, bR1Max-2}],
   Table[IBPset0R1[[3]] /. repvar /. reppara2N, {a, aR1Min, aR1Max-1}, {b1, bR1Min, bR1Max-2}, {b2, bR1Min+2, bR1Max}]
 ] // id // symmetry // Flatten // DeleteDuplicates;
+]
+
 
 IBPset1 = Join[IBPset1G, IBPset1R1] // DeleteDuplicates;
-]
 Print["Raw Top Sector (G) Equation Count: ", Length[IBPset1G]];
 Print["Raw R1 Sector Equation Count: ", Length[IBPset1R1]];
 
@@ -206,17 +208,18 @@ Print["Applying power selection and Expand..."];
 AbsoluteTiming[
 IBPsetPG = ReplaceAll[IBPset1G, reppowerselection] // Expand // DeleteDuplicates // Complement[#, {0}] &;
 IBPsetPR1 = ReplaceAll[IBPset1R1, reppowerselection] // Expand // DeleteDuplicates // Complement[#, {0}] &;
-IBPsetP = Join[IBPsetPG, IBPsetPR1] // DeleteDuplicates;
+(* --------select the part you want to reduce---------------*)
+IBPsetP0 = Join[IBPsetPG, IBPsetPR1] // DeleteDuplicates;
 ]
 Print["Selected Top Sector (G) Equation Count: ", Length[IBPsetPG]];
 Print["Selected R1 Sector Equation Count: ", Length[IBPsetPR1]];
-Print["Final Equation Count: ", Length[IBPsetP]];
+Print["Final Equation Count: ", Length[IBPsetP0]];
 
-varsetAll = IBPsetP // Cases[#, _R1 | _G, Infinity] & // DeleteDuplicates;
+varsetAll = IBPsetP0 // Cases[#, _R1 | _G, Infinity] & // DeleteDuplicates;
 Print["Final Integral Count: ", Length[varsetAll]];
 
 
-(* ::Section:: *)
+(* ::Section::Closed:: *)
 (*Constructing Master Integrals (MIs) & dlog Basis*)
 
 
@@ -259,7 +262,7 @@ MIdlogTopNote = Join[basis3DTopNote, basis1D3DTopNote] /. reppowerselection // C
 vec3Da0 = MIsR1 /. {a -> 0, b1 -> 2, b2 -> 2};
 vec3Da1 = MIsR1 /. {a -> 1, b1 -> 2, b2 -> 1};
 vec3Da0t = MIsR1 /. {a -> 0, b1 -> 2, b2 -> 1};
-OmegaInfinityRef = -(Omegax2Ref + OmegaPlusRef + OmegaMinusRef);
+OmegaInfinityRef = -(Omegax2Ref + OmegaPlusRef + OmegaMinusRef)//Simplify;
 
 vec3Da1Corr = vec3Da1 + (I/P0R1) * OmegaP0Ref . (MIsR1 /. {a -> 0, b1 -> 2, b2 -> 1}) // Simplify;
 vec3Da0tCorr = (2/P0R1) * (((b20 - 1) IdentityMatrix[Length[MIsR1]] - OmegaInfinityRef) . vec3Da0t) // Simplify;
@@ -290,7 +293,7 @@ MIdlogNote = Join[MIdlogTopNote, MIdlogR1Note];
 MIdlogKira = (MIdlogNote /. repvar /. reppara2N) // Simplify;
 
 
-(* ::Section:: *)
+(* ::Section::Closed:: *)
 (*Derivatives (k0, ks)*)
 
 
@@ -345,8 +348,15 @@ DEks0 = (dks[MIdlogNote] // id // symmetry);
 (*Formatting Variables & Target for Kira*)
 
 
-IBPsetdlogbasis = Table[G["dlog", i] - MIdlogKira[[i]], {i, Length[MIdlogKira]}];
-IBPsetP = Join[IBPsetdlogbasis, IBPsetP];
+(*\:68c0\:9a8c\:5fae\:5206\:65b9\:7a0b\:4e2d\:51fa\:73b0\:7684\:79ef\:5206\:662f\:5426\:5df2\:7ecf\:5305\:542b\:5728IBP\:6307\:6807\:8303\:56f4\:5185*)
+test=(*{DEk00, DEks0}*)MIdlogKira// Cases[#, _R1 | _G, Infinity] & // DeleteDuplicates;
+test/.G[c1_,c2_,c3_]->G[Total[c2],Total[c3]]/.R1[c1_,c2_,c3_]->R1[Total[c2],Total[c3]]//DeleteDuplicates
+Complement[test,varsetAll]
+test=.
+
+
+IBPsetdlogbasis =(*{}*)Table[G["dlog", i] - MIdlogKira[[i]], {i, Length[MIdlogKira]}];
+IBPsetP = Join[IBPsetdlogbasis, IBPsetP0];
 
 varsetP = Join[IBPsetP, {DEk00, DEks0}] // Cases[#, _R1 | _G, Infinity] & // DeleteDuplicates;
 (* Sorting rule for varsetP (integral ordering for Kira):
@@ -362,7 +372,7 @@ varsetP = Join[IBPsetP, {DEk00, DEks0}] // Cases[#, _R1 | _G, Infinity] & // Del
 varsetP = SortBy[varsetP, Which[
     MatchQ[#, G["dlog", _Integer]],
       {0, #[[2]], 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    MatchQ[#, G[__]],
+    MatchQ[#, G[_,_,_]],
       With[{n1=#[[1,1]], n2=#[[1,2]], n3=#[[1,3]], n4=#[[1,4]],
             a1=#[[2,1]], a2=#[[2,2]], b1=#[[3,1]], b2=#[[3,2]]},
         {1, Total[Abs /@ {a1,a2,b1,b2}] + If[a1<0||a2<0||b1<0||b2<0, 400, 0],
@@ -377,15 +387,18 @@ varsetP = SortBy[varsetP, Which[
   ] &
 ];
 
-repkira2R1 = Table[Tuserweight[i] -> varsetP[[i]], {i, Length[varsetP]}];
-repR12num = Table[varsetP[[i]] -> Tuserweight[i], {i, Length[varsetP]}];
+repkira2GR = Table[Tuserweight[i] -> varsetP[[i]], {i, Length[varsetP]}];
+repGR2num = Table[varsetP[[i]] -> Tuserweight[i], {i, Length[varsetP]}];
+
+
+repGR2num
 
 
 (* ::Section:: *)
 (*Exporting Kira Workspace*)
 
 
-kiraworkspace = "kira_bubble";
+kiraworkspace = "kira_bubble";  
 If[!DirectoryQ["./" <> kiraworkspace], CreateDirectory["./" <> kiraworkspace, CreateIntermediateDirectories -> True]];
 If[!DirectoryQ["./" <> kiraworkspace <> "/userSystem"], CreateDirectory["./" <> kiraworkspace <> "/userSystem", CreateIntermediateDirectories -> True]];
 If[!DirectoryQ["./" <> kiraworkspace <> "/result"], CreateDirectory["./" <> kiraworkspace <> "/result", CreateIntermediateDirectories -> True]];
@@ -399,7 +412,7 @@ ctokb[IBPset0_, varset0_] := Module[{string, stream, intsettemp, indexset},
   Do[
     string = "";
     intsettemp = IBPset0[[i]] // Cases[#, _R1 | _G, Infinity] & // DeleteDuplicates;
-    indexset = Table[intsettemp[[j]] /. repR12num /. Tuserweight[c_] -> c, {j, Length[intsettemp]}] // Flatten;
+    indexset = Table[intsettemp[[j]] /. repGR2num /. Tuserweight[c_] -> c, {j, Length[intsettemp]}] // Flatten;
     Do[
       string = StringJoin[string,
         ToString[indexset[[k]]], "*(",
@@ -417,12 +430,10 @@ ctokb[IBPset0_, varset0_] := Module[{string, stream, intsettemp, indexset},
 (*ctokb[IBPsetP, varsetP];*)
 
  (*--- Numeric test run: uncomment the block below AND comment the line above ---*)
-repNumericTestKira = {nu -> 3/7, ep -> 1/13, k0 -> 5/23};
+repNumericTestKira = {nu -> 17/7, ep -> 1/13, k0 -> 5/23};
 IBPsetPKira = IBPsetP /. repNumericTestKira;
-repR12numBak = repR12num;
-repR12num = Table[(varsetP[[i]] /. repNumericTestKira) -> Tuserweight[i], {i, Length[varsetP]}];
+repGR2num = Table[(varsetP[[i]] /. repNumericTestKira) -> Tuserweight[i], {i, Length[varsetP]}];
 ctokb[IBPsetPKira, varsetP];
-repR12num = repR12numBak;
 
 (* Append dummy equation: (N+1)*(ccc) \[LongDash] only needed when ALL coefficients are numeric.
    For symbolic run (nu, ep, k0 in coefficients), not needed but harmless. *)
@@ -450,8 +461,8 @@ listb[targetK0_] := Module[{string, stream},
    Only integrals in this list will have reduction rules in kira_list.m.
    Set targetMax to cover at least all integrals appearing in derivatives of dlog basis.
    Use Length[varsetP] to reduce all; use a smaller number for faster test runs. *)
-targetMax = 3000;  (* <-- adjust as needed; must cover all integrals in dk0/dks of dlog basis *)
-targetKAll = Range[Min[targetMax, Length[varsetP]] + 1];  (* +1 for dummy integral in numeric mode *)
+targetMax = 800;  (* <-- adjust as needed; must cover all integrals in dk0/dks of dlog basis *)
+targetKAll = Range[Min[targetMax, Length[varsetP]+ 1]];  (* +1 for dummy integral in numeric mode *)
 listb[targetKAll];
 Print["list written (", Length[targetKAll], " target integrals)."];
 
@@ -492,10 +503,17 @@ Export["./" <> kiraworkspace <> "/run.sh",
 
 (* === Export result files for bubble_de.m === *)
 Export["./" <> kiraworkspace <> "/result/MIdlogNote.m", MIdlogNote];
-Export["./" <> kiraworkspace <> "/result/repkira2R1.m", repkira2R1];
-Export["./" <> kiraworkspace <> "/result/repR12num.m", repR12num];
+Export["./" <> kiraworkspace <> "/result/repkira2GR.m", repkira2GR];
+Export["./" <> kiraworkspace <> "/result/repGR2num.m", repGR2num];
 Save["./" <> kiraworkspace <> "/result/derivative_rules_bubble.m",
   {listcal, repvar, reppara2N, repNumericTestKira, id, symmetry, dk0, dks, dk0Term, dksTerm}];
 
 Print["Export done. Workspace ready at: ", kiraworkspace];
 
+
+
+(* ::Section:: *)
+(*test*)
+
+
+Tuserweight[16]/.repkira2GR
